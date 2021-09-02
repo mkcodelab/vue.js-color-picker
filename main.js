7af80259d395f151168a2b00699b6da805d685dd
mikcodelab <michal.kulawik@gmail.com>
2021-09-02 16:55:51 +0200

    refactored rgba picker.

--- main.js
+++ main.js
@@ -38,18 +38,7 @@ const app = Vue.createApp({
       this.hexaCard = true;
     },
     rgbUpdateVal() {
-      let bars = document.querySelectorAll('.rangebar');
-      // use destructuring
-      let r = bars[0].value;
-      let g = bars[1].value;
-      let b = bars[2].value;
-      let a = bars[3].value;
-      let rgba = `rgba(${r},${g},${b},${a})`
-      this.rgbaVal = rgba;
-      this.rgba.r = r;
-      this.rgba.g = g;
-      this.rgba.b = b;
-      this.rgba.a = a;
+      this.rgbaVal = `rgba(${this.rgba.r}, ${this.rgba.g}, ${this.rgba.b}, ${this.rgba.a})`;
     },
     hslaUpdateVal() {
       this.hslaVal = `hsla(${this.hsla.h}, ${this.hsla.s}%, ${this.hsla.l}%, ${this.hsla.a})`;
